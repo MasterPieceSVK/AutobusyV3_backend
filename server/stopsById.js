ee0b72a4db7ccef3a95bb2a31addc4bfceecc0e6
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-02-06 17:04:18 +0100

    e

--- server/stopsById.js
+++ server/stopsById.js
@@ -5,6 +5,9 @@ const axios = require("axios");
 
 stopsByIdRouter.post("/:stop_id/:time", (req, res) => {
   const { stop_id, time } = req.params;
+  if (time == 2) {
+    time = 0;
+  }
   let date = new Date();
   date.setHours(date.getHours() - time);
 
